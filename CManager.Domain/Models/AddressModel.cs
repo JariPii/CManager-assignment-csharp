f9d87ba94cf1f8f6665cab7be30ae95cfb6a0d8e
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2025-12-08 13:25:11 +0100

    Adds Customer and Address Models

---
+++ CManager.Domain/Models/AddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CManager.Domain.Models
+{
+    internal class AddressModel
+    {
+        [StringLength(50)]
+        public required string StreetAddress { get; set; }
+
+        [StringLength(50)]
+        public required string ProstalCode { get; set; }
+
+        [StringLength(50)]
+        public required string City { get; set; }
+    }
+}
---
+++ CManager.Domain/Models/CustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CManager.Domain.Models
+{
+    internal class CustomerModel
+    {
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+        [StringLength(50)]
+        public required string LastName { get; set; }
+        [StringLength(50)]
+        public required string Email { get; set; }
+        [StringLength(20)]
+        public required string PhoneNumber { get; set; }
+
+        public required AddressModel Address { get; set; }
+
+    }
+}
