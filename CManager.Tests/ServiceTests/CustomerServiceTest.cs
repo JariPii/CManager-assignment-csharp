bd281953f55498e0a8d95b8dadb94416ea162a25
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-01-11 17:17:27 +0100

    Adds delete customer test

---
+++ CManager.Tests/ServiceTests/CustomerServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CManager.Application.Services;
+using CManager.Infrastructure.Repositories;
+using CManager.Domain.Models;
+using Moq;
+
+namespace CManager.Tests.ServiceTests
+{
+    public class CustomerServiceTest
+    {
+        [Fact]
+        public void DeleteCustomer_WithEmptyGuid_ReturnFalse()
+        {
+            var mockCustomerRepo = new Mock<ICustomerRepository>();
+            mockCustomerRepo.Setup(r => r.GetAllCustomers()).Returns(new List<CustomerModel>());
+
+            var service = new CustomerService(mockCustomerRepo.Object);
+
+            var result = service.DeleteCustomer(Guid.Empty);
+
+            Assert.False(result);
+            mockCustomerRepo.Verify(r => r.GetAllCustomers(), Times.Never);
+        }
+
+        [Fact]
+    }
+}
